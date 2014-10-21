ea4fe2f922e1c70211de397048ff6e32bb9b6b28
austinforlife <austin-washburn@uiowa.edu>
2014-10-21 12:35:12 -0500

    FieldSearch class start
    Hmmmmm……

---
+++ FieldSearch.java
@@ -0,0 +1,21 @@
+package org.uiowa.cs2820.engine;
+
+import java.util.ArrayList;
+
+public class FieldSearch {
+
+
+	public FieldSearch() {
+		ArrayList<Field> T = new ArrayList<Field>;
+	}
+
+	public void FindEquals() {
+		//communicate with the Field class, to getFieldName(),getFieldValue()
+		
+		//use the FieldName and Value to search Database
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+}
