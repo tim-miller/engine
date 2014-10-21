fc5e7c64f0a0800975a6dd6893b1c1a918e27449
austinforlife <austin-washburn@uiowa.edu>
2014-10-21 13:00:22 -0500

    moving forward

--- FieldSearch.java
+++ FieldSearch.java
@@ -10,12 +10,14 @@ public class FieldSearch extends Field{
 	public FieldSearch() {
 		ArrayList<Field> T = new ArrayList<Field>;
 	}
-
+	
+//being passed a field
 	public void FindEquals() {
+
 		//communicate with the Field class, to getFieldName(),getFieldValue()
-		Database Y = new Database()
+		T.getFromIndex //
 		//use the FieldName and Value to search Database
-		
+//should return an array of string results for matches found in database		
 	}
 	public static void main(String[] args) {
 		
