6a0b3906c1820c43cc1cb8dced0bb8b7373eb656
Tim Miller <tim-miller@uiowa.edu>
2014-10-23 11:18:26 -0500

    Field Class
    Field

--- Field.java
+++ Field.java
@@ -1,14 +1,46 @@
+/*
+ 
+ Tim Miller / Field Class
+ 
+ Group 4
+ ----------------------
+ Carter Davis: Indexer
+ Chung Meng Chua: Database
+ Austin Washburn: FieldSearch
+ Tim Miller: Field
+ 
+ */
+
+
 package org.uiowa.cs2820.engine;
 
 public class Field {
-
-	public Field(String FieldName, String FieldValue) {
-		return;
-	    }
-	public String getFieldName() {
-	    return "";  
-	    }
-	public String getFieldValue() {
-		return "";
-	    }
+    
+	private String Name;
+	private String Value;
+    
+    //constructor
+	Field(String FieldName, String FieldValue) {
+		Name = FieldName;
+		Value = FieldValue;
+	}
+    
+    //getters
+	String getFieldName() {
+		return Name;
+	}
+    
+	String getFieldValue() {
+		return Value;
+	}
+    
+    //setters
+	void setFieldName(String a) {
+		Name = a;
+	}
+    
+	void setFieldValue(String b) {
+		Value = b;
+        
+	}
 }
